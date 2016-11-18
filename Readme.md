30a6e5eec85edefc2c52f053374af003fef5565e
Andrew Coats <acoats@calicosol.com>
2016-11-17 18:55:51 -0700

    Add SpiderController.

---
+++ Readme.md
@@ -0,0 +1,6 @@
+
+## SpiderTrap
+
+* An ASP.NET implementation of [SpiderTrap](https://github.com/adhdproject/adhdproject.github.io/blob/master/Tools/Spidertrap.md).
+* use `wget -m http://127.0.0.1:60170 -nH` to test.
+  
---
+++ src/SpiderTrapWebApplication/Controllers/SpiderController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpiderTrapWebApplication.Controllers
+{
+    public class SpiderController : Controller
+    {
+        [Route("Spider/{id}")]
+        [Route("Spider")]
+        public IActionResult Index(string id = "test")
+        {
+            var newId = Guid.NewGuid().ToString();
+            var html = new StringBuilder();
+            html.Append("<html><head></head><body>");
+            html.Append($"<a href='/spider/{newId}'>{newId}</a>");
+            html.Append("</body></html>");
+            return Content(html.ToString(), "text/html");
+        }
+    }
+}
